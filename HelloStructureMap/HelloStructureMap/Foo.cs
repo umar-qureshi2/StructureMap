f56983f54ab15cd88819a5c46763dfb6afa44661
umar-qureshi2 <umar.mcs@gmail.com>
2018-05-28 14:49:36 +0500

    Initial commit

---
+++ HelloStructureMap/HelloStructureMap/Foo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStructureMap
+{
+    public interface IBar
+    {
+    }
+
+    public class Bar : IBar
+    {
+    }
+
+    public interface IFoo
+    {
+    }
+
+    public class Foo : IFoo
+    {
+        public IBar Bar { get; private set; }
+
+        public Foo(IBar bar)
+        {
+            Bar = bar;
+        }
+    }
+}
---
+++ HelloStructureMap/HelloStructureMap/Program.cs
@@ -0,0 +1,49 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStructureMap
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Configure and build a brand new
+            // StructureMap Container object
+            var container = new Container(_ =>
+            {
+                _.For<IFoo>().Use<Foo>();
+                _.For<IBar>().Use<Bar>();
+            });
+
+            //// Now, resolve a new object instance of IFoo
+            //container.GetInstance<IFoo>()
+            //    // should be type Foo
+            //    .ShouldBeOfType<Foo>()
+
+            //    // and the IBar dependency too
+            //    .Bar.ShouldBeOfType<Bar>();
+
+            var containerScan = new Container(_ =>
+            {
+                _.Scan(x =>
+                {
+                    x.TheCallingAssembly();
+                    x.WithDefaultConventions();
+                });
+            });
+
+            var foo = container.GetInstance<Foo>();
+            var bar = foo.Bar;
+
+            Console.WriteLine(foo);
+            Console.WriteLine(bar);
+
+
+        }
+    }
+}
